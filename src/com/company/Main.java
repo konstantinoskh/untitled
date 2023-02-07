fe8f54a3f17ae096c54f9043b434af1aec3f3baf
konstantinoskh <17KHELTUBNISHVILI_K@bexleygs.co.uk>
2023-02-07 09:25:18 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    int[] data = {13, 13, 18, 19, 23, 38, 57, 70, 79, 88, 89, 94, 102, 108, 122, 125, 127, 177, 180, 201, 210, 221, 226, 226, 234, 237, 247, 293, 295, 297, 303, 317, 320, 339, 343, 373, 375, 378, 378, 380, 403, 405, 407, 408, 412, 440, 466, 479, 485, 486, 493, 504, 509, 525, 537, 564, 565, 570, 578, 581, 584, 589, 605, 611, 617, 621, 650, 665, 669, 692, 696, 714, 742, 756, 756, 763, 766, 776, 780, 789, 817, 857, 881, 882, 883, 885, 897, 898, 906, 909, 911, 916, 929, 929, 939, 941, 958, 985, 988, 994};
+
+    public  int searchNumber(int n, int[] numArray){
+        numArray = data;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Is your number higher or lower than " + numArray[(numArray.length)/2] + "?: ");
+        String answer = sc.nextLine().toUpperCase();
+
+        if (answer.equals("L")){
+            searchNumber(n, numArray)
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
